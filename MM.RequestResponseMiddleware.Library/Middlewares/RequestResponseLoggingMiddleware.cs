ced6924e3e431754c7deb0e2ca8550877f390462
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-18 16:58:38 +0400

    Added middleware part 1

--- MM.RequestResponseMiddleware.Library/Middlewares/RequestResponseLoggingMiddleware.cs
+++ MM.RequestResponseMiddleware.Library/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.IO;
+using MM.RequestResponseMiddleware.Library.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -11,10 +14,82 @@ namespace MM.RequestResponseMiddleware.Library.Middlewares
     public class RequestResponseLoggingMiddleware
     {
         private readonly RequestDelegate next;
+        private readonly RequestResponseOptions requestResponseOptions;
+        private readonly RecyclableMemoryStreamManager recyclableMemoryStreamManager;
 
-        public async Task Invoke(HttpContext context )
+        public RequestResponseLoggingMiddleware(RequestDelegate next, RequestResponseOptions requestResponseOptions, RecyclableMemoryStreamManager recyclableMemoryStreamManager)
         {
-            await next(context);
+            this.next = next;
+            this.requestResponseOptions = requestResponseOptions;
+            this.recyclableMemoryStreamManager = recyclableMemoryStreamManager;
         }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var requestBody = await GetRequestBody(context);
+
+            var originalBodyStream = context.Response.Body;
+
+            await using var responseBody = recyclableMemoryStreamManager.GetStream();
+            context.Response.Body = responseBody;
+
+            var stopWatch = Stopwatch.StartNew();
+
+            await next(context);
+
+            stopWatch.Stop();
+
+            context.Request.Body.Seek(0, SeekOrigin.Begin);
+            string responseBodyText = await new StreamReader(context.Response.Body).ReadToEndAsync();
+            context.Request.Body.Seek(0, SeekOrigin.Begin);
+
+
+            var requestResponseContext = new RequestResponseContext(context)
+            {
+                RequestBody = requestBody,
+                ResponseCreationTime = TimeSpan.FromTicks(stopWatch.ElapsedTicks),
+                ResponseBody = responseBodyText,
+            };
+
+
+            requestResponseOptions.RequestResponseHandler?.Invoke(requestResponseContext);
+        }
+
+        private static string ReadStreamInChunks(Stream stream)
+        {
+            const int readChunkBufferLength = 0;
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using var textWriter = new StringWriter();
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+
+            var readChunk = new char[readChunkBufferLength];
+            int readChunkLength;
+
+            do
+            {
+                readChunkLength = reader.Read(readChunk, 0, readChunkBufferLength);
+                textWriter.Write(readChunk, 0, readChunkLength);
+            }
+            while (readChunkLength > 0);
+
+            return textWriter.ToString();
+        }
+
+        private async Task<string> GetRequestBody(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+
+            await using var requestStream = recyclableMemoryStreamManager.GetStream();
+            await context.Request.Body.CopyToAsync(requestStream);
+
+            string requestBody = ReadStreamInChunks(requestStream);
+
+            context.Request.Body.Seek(0, SeekOrigin.Begin);
+
+            return requestBody;
+        }
+
     }
 }
