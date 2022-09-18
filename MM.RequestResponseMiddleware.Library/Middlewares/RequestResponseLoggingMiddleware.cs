b9bec62a7a7f8f7e0109858f85842881a1269d56
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-18 16:05:26 +0400

    Add project files.

---
+++ MM.RequestResponseMiddleware.Library/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM.RequestResponseMiddleware.Library.Middlewares
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public async Task Invoke(HttpContext context )
+        {
+            await next(context);
+        }
+    }
+}
